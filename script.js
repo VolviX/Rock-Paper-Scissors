4e0a2a8e043f039da7255f76c565bc1901962f57
VolviX <20700808+VolviX@users.noreply.github.com>
2024-11-10 17:31:55 +0300

    Add playRound function

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Generates random integer: 0 to max - 1
-function getRandomInt(max) {
-
-    return Math.floor(Math.random() * max);
-  }
-
-// Generates computer choice
-function getComputerChoice() {
-    let computer = getRandomInt(3);
-
-    if (computer == 0) {
-        console.log("COMPUTER: ROCK");
-
-    } else if (computer == 1) {
-        console.log("COMPUTER: PAPER");
-
-    } else {
-        console.log("COMPUTER: SCISSORS");
-    }
-}
-
 // Asks user input 
 function getHumanChoice() {
     let input;
@@ -40,7 +19,57 @@ function getHumanChoice() {
             console.log("Invalid choice, try again.");
         }
     } 
+
+    input = +input; // Converts to number because prompt() gets string
+    return input;
 }
 
-getHumanChoice();
-getComputerChoice();
+// Generates random integer: 0 to max - 1
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+// Generates computer choice
+function getComputerChoice() {
+    let computer = getRandomInt(3);
+
+    if (computer == 0) {
+        console.log("COMPUTER: ROCK");
+
+    } else if (computer == 1) {
+        console.log("COMPUTER: PAPER");
+
+    } else {
+        console.log("COMPUTER: SCISSORS");
+    }
+
+    return computer;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(getHumanChoice, getComputerChoice) {
+    console.log("Initializing the game...");
+
+    let user = getHumanChoice();
+    let ai = getComputerChoice();
+
+    if (user === ai) {
+        console.log("It's a tie!");
+    } else if (user === 0 && ai === 1) {
+        console.log("Computer wins this round!");
+    } else if (user === 0 && ai === 2) {
+        console.log("User wins this round!");
+    } else if (user === 1 && ai === 0) {
+        console.log("User wins this round!");
+    } else if (user === 1 && ai === 2) {
+        console.log("Computer wins this round!");
+    } else if (user === 2 && ai == 0) {
+        console.log("Computer wins this round!");
+    } else {
+        console.log("User wins this round!");
+    }
+}
+
+playRound(getHumanChoice, getComputerChoice);
